3297d1484e40fa2728728c51767edd85ffe4ffcb
tcase360 <tmcase16@gmail.com>
2017-06-07 14:28:31 +0200

    Add description without partition

--- implementations/sorting/quick-sort.js
+++ implementations/sorting/quick-sort.js
@@ -1,6 +1,21 @@
 /**
  * Quick sort algorithm
  *
+ * This is one of the more efficient sorting algorithms,
+ * making it rather important. It's called a "divide & conquer"
+ * algorithm, which makes it sound pretty awesome. Here's how
+ * it works:
+ *
+ * First, pick a partition. In the code, the variable will be called
+ * a "pivot". The basic implementation will start with
+ * the first element in the array, but there are other ways to
+ * determine this which we will get into later.
+ *
+ * Basically, the algorithm will take the partition and
+ * compare it with the rest of the array, and any elements
+ * less than the partition will go into the "lesser" array,
+ * any elements greater than the partition will go into the
+ * "greater" array
  *
  */
 
