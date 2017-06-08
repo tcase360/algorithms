b7ebfe67dc45d4ab37b497ac2094d56f41690321
tcase360 <tmcase16@gmail.com>
2017-06-08 10:12:48 +0200

    add implementation and test file

---
+++ implementations/sorting/merge-sort.js
@@ -0,0 +1,21 @@
+import {
+  swap,
+  randomArray,
+  generateOrderedArray,
+  randomizeArray,
+ } from '../communal-functions';
+
+ class MergeSort {
+   constructor() {
+     this.randomArray = randomArray(1000);
+     this.counters = {
+       countOuter: 0,
+       countInner: 0,
+       countSwap: 0
+     }
+   }
+
+   static basicImplementation(array) {
+     
+   }
+ }
---
+++ tests/merge-sort.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+import MergeSort from '../implementations/sorting/merge-sort';
+
+const unsortedArray = [4, 3, 5, 8, 6, 7, 9, 10, 0, 2, 1];
+const sortedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+describe('Array', function() {
+  describe('#MergeSort.basicImplementation', function() {
+    it('should return a sorted array', function() {
+      assert.equal(sortedArray.join(','), MergeSort.basicImplementation(unsortedArray).join(','));
+    });
+  });
+});
+
+describe('Array', function() {
+  describe('#MergeSort.optimizedImplementation', function() {
+    it('should return a sorted array', function() {
+      assert.equal(sortedArray.join(','), MergeSort.optimizedImplementation(unsortedArray).join(','));
+    });
+  });
+});
