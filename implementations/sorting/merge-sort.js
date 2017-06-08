9b373433c2f14e4a058830367fa7e7a963bfaddb
tcase360 <tmcase16@gmail.com>
2017-06-08 12:32:12 +0200

    finish methods for bottom up sort

--- implementations/sorting/merge-sort.js
+++ implementations/sorting/merge-sort.js
@@ -43,11 +43,41 @@ import {
    }
 
    static bottomUp(array) {
+     let step = 1;
+     while (step < array.length) {
+       let left = 0;
+       while (left + step < array.length) {
+         this.bottomUpMerge(array, left, step);
+         left += step * 2;
+       }
+       step *= 2;
+     }
 
+     return array;
    }
 
-   static bottomUpMerge(left, right) {
+   static bottomUpMerge(array, left, step) {
+     let right = left + step;
+     let end = Math.min(left + step * 2 - 1, array.length - 1);
+     let leftMoving = left;
+     let rightMoving = right;
+     let temp = [];
 
+     for (var i = left; i <= end; i++) {
+       if ((array[leftMoving] <= array[rightMoving] || rightMoving > end) &&
+            leftMoving < right) {
+          temp[i] = array[leftMoving];
+          leftMoving++;
+       } else {
+         temp[i] = array[rightMoving];
+         rightMoving++;
+       }
+     }
+
+     for (var j = left; j <= end; j++) {
+       array[j] = temp[j];
+     }
+     return array;
    }
  }
 
--- tests/merge-sort.test.js
+++ tests/merge-sort.test.js
@@ -5,17 +5,17 @@ const unsortedArray = [4, 3, 5, 8, 6, 7, 9, 10, 0, 2, 1];
 const sortedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 describe('Array', function() {
-  describe('#MergeSort.basicImplementation', function() {
+  describe('#MergeSort.topDown', function() {
     it('should return a sorted array from high to low', function() {
       assert.equal(sortedArray.reverse().join(','), MergeSort.topDown(unsortedArray).join(','));
     });
   });
 });
 
-// describe('Array', function() {
-//   describe('#MergeSort.optimizedImplementation', function() {
-//     it('should return a sorted array', function() {
-//       assert.equal(sortedArray.join(','), MergeSort.bottomUp(unsortedArray).join(','));
-//     });
-//   });
-// });
+describe('Array', function() {
+  describe('#MergeSort.bottomUp', function() {
+    it('should return a sorted array', function() {
+      assert.equal(sortedArray.reverse().join(','), MergeSort.bottomUp(unsortedArray).join(','));
+    });
+  });
+});
